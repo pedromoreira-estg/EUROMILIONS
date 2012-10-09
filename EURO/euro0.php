4e7f70b0c10fe4eb8c084633778eb3e33474505f
Pedro Moreira <pmoreira.estg@gmail.com>
2012-10-09 20:21:39 +0100

    initial version

---
+++ EURO/euro0.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>
+			EuroMilhões
+		</title>
+		<link rel="stylesheet" href="css/euro.css" /> 
+	</head>
+	<body>
+		<div class="chave">
+		<ul class="numeros">
+			<li><?php echo rand(1,50);?></li>
+			<li><?php echo rand(1,50);?></li>
+			<li><?php echo rand(1,50);?></li>
+			<li><?php echo rand(1,50);?></li>
+			<li><?php echo rand(1,50);?></li>
+		</ul>
+		<ul class="estrelas">
+			<li><?php echo rand(1,11);?></li>
+			<li><?php echo rand(1,11);?></li>
+		</ul>
+		</div>
+	</body>
+</html>
+
